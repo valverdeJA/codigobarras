7f30b519dc936f33f557816a91358c3f5bd34bd9
valverdeJA <valverde.ja@gmail.com>
2025-01-31 17:27:34 +0100

    12312dswad

--- live_w_locator.js
+++ live_w_locator.js
@@ -288,10 +288,9 @@ $(function() {
             App.lastResult = code;
             var $node = null, canvas = Quagga.canvas.dom.image;
 
-            $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
-            $node.find("img").attr("src", canvas.toDataURL());
-            $node.find("h4.code").html(code);
-            $("#result_strip ul.thumbnails").prepend($node);
+        // Muestra el resultado en texto plano dentro del div result_strip
+        var resultStrip = document.getElementById("result_strip");
+        resultStrip.innerHTML = "<p>Código detectado: <strong>" + code + "</strong></p>";
         }
     });
 
